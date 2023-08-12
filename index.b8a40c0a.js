e23e607d444cf075ffb6f51840df17f7ba1953ad
Svatosik <125908175+Svatosik@users.noreply.github.com>
2023-08-12 19:24:14 +0000

    Deploying to gh-pages from @ Svatosik/goit-js-hw-011@e36b07410851fb0737752e73354c2dc4d3b563a2 🚀

---
+++ index.b8a40c0a.js
@@ -0,0 +1,2 @@
+var upScrollLink=document.querySelector(".scroll-up");window.addEventListener("scroll",(function(){window.scrollY>100?upScrollLink.style.display="flex":upScrollLink.style.display="none"}));
+//# sourceMappingURL=index.b8a40c0a.js.map
